a51436fe383a692dfdc5bb04fdda2d606e77d54c
18758 <44072429@qq.com>
2018-06-23 11:48:05 +0800

    android util

--- app/src/main/java/util/android/ys/com/androidutil/MainActivity.java
+++ app/src/main/java/util/android/ys/com/androidutil/MainActivity.java
@@ -1,29 +1,98 @@
 package util.android.ys.com.androidutil;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.ArrayMap;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import util.android.ys.com.util.HttpClientUtil;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private String message = null;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String obj = (String) msg.obj;
+            switch (msg.what) {
+                case 1:
+                    Toast.makeText(MainActivity.this, obj, Toast.LENGTH_SHORT).show();
+                    break;
+                case 2:
+                    Toast.makeText(MainActivity.this, obj, Toast.LENGTH_SHORT).show();
+                    break;
+                case 3:
+                    Toast.makeText(MainActivity.this, obj, Toast.LENGTH_SHORT).show();
+
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @OnClick({R.id.Btn1, R.id.Btn2, R.id.Btn3})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.Btn1:
+                Toast.makeText(MainActivity.this, "aaaa", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Btn2:
 
-//    @OnClick({R.id.Btn1, R.id.Btn2, R.id.Btn3})
-//    public void onViewClicked(View view) {
-//        switch (view.getId()) {
-//            case R.id.Btn1:
-//                break;
-//            case R.id.Btn2:
-//                break;
-//            case R.id.Btn3:
-//                break;
-//        }
-//    }
+                Toast.makeText(MainActivity.this, "aaaa", Toast.LENGTH_SHORT).show();
 
+                Map test = new ArrayMap<String, String>();
+                test.put("topic" , "appservice/checkversion");
+                test.put("dtoMessage" , "{     \"devid\": \"d0d8641757b930d5\",\"package\": \"com.abjc.abp.abp_abjc\",     \"versionCode\": 122,     \"versionName\": \"1.0.0.122nx\" }");
+
+                HttpClientUtil.post2("http://192.168.21.126:8080/abp/aa/index.jhtml", test, new HttpClientUtil.HttpClientEventListener() {
+                    @Override
+                    public void onSuccess(int status_code, byte[] bytes) {
+                        Message mes = new Message();
+                        mes.what = 1;
+                        mes.obj = "成功";
+                        handler.sendMessage(mes);
+                    }
+
+                    @Override
+                    public void onFailure(int status_code, byte[] bytes) {
+                        Message mes = new Message();
+                        mes.what = 2;
+                        mes.obj = "失败";
+                        handler.sendMessage(mes);
+                    }
+
+                    @Override
+                    public void onTimeout() {
+                        Message mes = new Message();
+                        mes.what = 3;
+                        mes.obj = "超时";
+                        handler.sendMessage(mes);
+                    }
+                }, 40000);
+
+                break;
+            case R.id.Btn3:
+                Toast.makeText(MainActivity.this, "aaaa", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
--- util/src/main/java/util/android/ys/com/util/HttpClientUtil.java
+++ util/src/main/java/util/android/ys/com/util/HttpClientUtil.java
@@ -10,6 +10,8 @@ import com.loopj.android.http.SyncHttpClient;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.ConnectException;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +22,7 @@ import cz.msebera.android.httpclient.HttpEntity;
 import cz.msebera.android.httpclient.HttpResponse;
 import cz.msebera.android.httpclient.NameValuePair;
 import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.config.RequestConfig;
 import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
 import cz.msebera.android.httpclient.client.methods.HttpPost;
 import cz.msebera.android.httpclient.conn.ConnectTimeoutException;
@@ -42,8 +45,8 @@ public class HttpClientUtil {
         void onTimeout();
     }
 
+    //使用异步Http发送get请求
     static public void get(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener) {
-        //使用异步HttpClient发送get请求
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams rp = new RequestParams(params);
         client.get(url, rp, new AsyncHttpResponseHandler() {
@@ -69,9 +72,8 @@ public class HttpClientUtil {
         });
     }
 
-
+    //使用异步Http发送post请求
     static public void post(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
-        //使用异步HttpClient发送post请求
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams rp = new RequestParams(params);
         client.setTimeout(timeout);
@@ -98,7 +100,7 @@ public class HttpClientUtil {
         });
     }
 
-//    // 同步的方式发送post请求
+    // 同步的方式发送post请求
 //    static public void postSync(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
 //
 //        SyncHttpClient client = new SyncHttpClient();
@@ -131,66 +133,6 @@ public class HttpClientUtil {
 //        });
 //    }
 
-//   public class postSync2Task implements Runnable {
-//
-//        private String url;
-//        private Map<String, String> params;
-//        private
-//
-//        postSync2Task(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
-//
-//        }
-//
-//        @Override
-//        public void run() {
-//            postSync2();
-//        }
-//
-//    }
-//
-//
-//    static public void post2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
-//        threadPool.submit();
-//    }
-
-
-    static public void postSync2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
-        try
-        {
-            List<NameValuePair> pairList = new ArrayList<NameValuePair>();
-            for (Map.Entry<String, String> param: params.entrySet()) {
-                pairList.add(new BasicNameValuePair(param.getKey(),param.getValue()));
-            }
-
-            HttpEntity requestHttpEntity = new UrlEncodedFormEntity(pairList);
-            // URL使用基本URL即可，其中不需要加参数
-            HttpPost httpPost = new HttpPost(url);
-            // 将请求体内容加入请求中
-            httpPost.setEntity(requestHttpEntity);
-            // 需要客户端对象来发送请求
-            HttpClient httpClient = new DefaultHttpClient();
-            // 发送请求
-            HttpResponse response = httpClient.execute(httpPost);
-
-            String data = EntityUtils.toString(response.getEntity());
-
-            httpClientEventListener.onSuccess(200, data.getBytes());
-
-            Log.d("response" , response.toString());
-
-        } catch (Exception error) {
-
-            if (error instanceof ConnectTimeoutException) {
-                httpClientEventListener.onTimeout();
-            } else if (error instanceof SocketTimeoutException) {
-                httpClientEventListener.onTimeout();
-            } else {
-                httpClientEventListener.onFailure(100, null);
-            }
-
-        }
-
-    }
 
     // 附件上传
     static public void testPostJsonSync(Context context, String url, Map<String, Object> params, final HttpClientEventListener httpClientEventListener, int timeout) {
@@ -234,8 +176,88 @@ public class HttpClientUtil {
                     }
                 }
             }
-
         });
+    }
+
+
+    /*
+    *   以下为 android 自带 http
+    * */
+
+    static class PostSync2Task implements Runnable {
+
+        private String url;
+        private Map<String, String> params;
+        private int timeout;
+        private HttpClientEventListener httpClientEventListener;
+
+        PostSync2Task(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
+
+            this.url = url;
+            this.params = params;
+            this.timeout = timeout;
+            this.httpClientEventListener = httpClientEventListener;
+
+        }
+
+        @Override
+        public void run() {
+            postSync2(url , params ,  httpClientEventListener , timeout);
+        }
+
+    }
+
+    // 异步 andriod 自带
+    static public void post2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
+        PostSync2Task postSync2Task = new PostSync2Task( url , params , httpClientEventListener , timeout);
+        new Thread(postSync2Task).start();
+    }
+
+
+    // 同步 andriod 自带 post
+    static public void postSync2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
+        try
+        {
+            List<NameValuePair> pairList = new ArrayList<NameValuePair>();
+            for (Map.Entry<String, String> param: params.entrySet()) {
+                pairList.add(new BasicNameValuePair(param.getKey(),param.getValue()));
+            }
+
+            HttpEntity requestHttpEntity = new UrlEncodedFormEntity(pairList);
+            // URL使用基本URL即可，其中不需要加参数
+            HttpPost httpPost = new HttpPost(url);
+            // 将请求体内容加入请求中
+            httpPost.setEntity(requestHttpEntity);
+
+            RequestConfig config=RequestConfig.custom()
+                             .setConnectTimeout(5000)       // 设置连接超时时间 5秒钟
+                             .setSocketTimeout(5000)        // 设置读取超时时间5秒钟
+                             .build();
+            httpPost.setConfig(config);
+
+            // 需要客户端对象来发送请求
+            HttpClient httpClient = new DefaultHttpClient();
+            // 发送请求
+            HttpResponse response = httpClient.execute(httpPost);
+
+            String data = EntityUtils.toString(response.getEntity());
+
+            httpClientEventListener.onSuccess(200, data.getBytes());
+
+            Log.d("response" , response.toString());
+
+        } catch (Exception error) {
+
+            if (error instanceof SocketException) {
+                httpClientEventListener.onTimeout();
+            } else if(error instanceof ConnectException){
+                httpClientEventListener.onTimeout();
+            }else {
+                httpClientEventListener.onFailure(100, null);
+            }
+
+        }
+
     }
 
 }
