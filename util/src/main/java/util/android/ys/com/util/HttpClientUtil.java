3a2bd7a1e44f791040b58b8e4020d26092615991
44072429 <44072429@qq.com>
2018-06-29 15:54:36 +0800

    android util
    Signed-off-by: 18758 <4407242922qq.com>

--- util/src/main/java/util/android/ys/com/util/HttpClientUtil.java
+++ util/src/main/java/util/android/ys/com/util/HttpClientUtil.java
@@ -5,45 +5,41 @@ import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.HttpGet;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.SyncHttpClient;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.ConnectException;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.net.ssl.SSLSocketFactory;
-
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.HttpEntity;
 import cz.msebera.android.httpclient.HttpResponse;
-import cz.msebera.android.httpclient.HttpVersion;
+import cz.msebera.android.httpclient.HttpStatus;
 import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.ClientProtocolException;
 import cz.msebera.android.httpclient.client.HttpClient;
 import cz.msebera.android.httpclient.client.config.RequestConfig;
 import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.CloseableHttpResponse;
 import cz.msebera.android.httpclient.client.methods.HttpPost;
-import cz.msebera.android.httpclient.conn.ClientConnectionManager;
 import cz.msebera.android.httpclient.conn.ConnectTimeoutException;
-import cz.msebera.android.httpclient.conn.scheme.Scheme;
-import cz.msebera.android.httpclient.conn.scheme.SchemeRegistry;
-import cz.msebera.android.httpclient.conn.ssl.TrustStrategy;
-import cz.msebera.android.httpclient.entity.StringEntity;
-import cz.msebera.android.httpclient.entity.mime.HttpMultipartMode;
+import cz.msebera.android.httpclient.entity.ContentType;
 import cz.msebera.android.httpclient.entity.mime.MultipartEntityBuilder;
+import cz.msebera.android.httpclient.entity.mime.content.FileBody;
+import cz.msebera.android.httpclient.entity.mime.content.StringBody;
+import cz.msebera.android.httpclient.impl.client.CloseableHttpClient;
 import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
-import cz.msebera.android.httpclient.impl.conn.PoolingClientConnectionManager;
+import cz.msebera.android.httpclient.impl.client.HttpClients;
 import cz.msebera.android.httpclient.message.BasicNameValuePair;
-import cz.msebera.android.httpclient.params.BasicHttpParams;
-import cz.msebera.android.httpclient.params.HttpParams;
-import cz.msebera.android.httpclient.params.HttpProtocolParams;
-import cz.msebera.android.httpclient.protocol.HTTP;
 import cz.msebera.android.httpclient.util.EntityUtils;
 
 /**
@@ -116,7 +112,7 @@ public class HttpClientUtil {
         });
     }
 
-    // 同步的方式发送post请求
+//    // 同步的方式发送post请求
 //    static public void postSync(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
 //
 //        SyncHttpClient client = new SyncHttpClient();
@@ -196,9 +192,9 @@ public class HttpClientUtil {
     }
 
 
-    /*
-    *   以下为 android 自带 http
-    * */
+    /**
+     * 以下为android 自带 Http请求
+     */
 
     static class PostSync2Task implements Runnable {
 
@@ -218,87 +214,230 @@ public class HttpClientUtil {
 
         @Override
         public void run() {
-            postSync2(url , params ,  httpClientEventListener , timeout);
+            postSync2(url, params, httpClientEventListener, timeout);
         }
 
     }
 
-    // 异步 andriod 自带
+
+    /**
+     * http 异步post请求
+     *
+     * @param url
+     * @param params
+     * @param httpClientEventListener
+     * @param timeout
+     */
     static public void post2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
-        PostSync2Task postSync2Task = new PostSync2Task( url , params , httpClientEventListener , timeout);
+        PostSync2Task postSync2Task = new PostSync2Task(url, params, httpClientEventListener, timeout);
         new Thread(postSync2Task).start();
     }
 
 
-    // 同步 andriod 自带 post
+    /**
+     * http 同步post请求
+     *
+     * @param url
+     * @param params
+     * @param httpClientEventListener
+     * @param timeout
+     */
     static public void postSync2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
-        try
-        {
+        try {
             List<NameValuePair> pairList = new ArrayList<NameValuePair>();
-            for (Map.Entry<String, String> param: params.entrySet()) {
-                pairList.add(new BasicNameValuePair(param.getKey(),param.getValue()));
+            for (Map.Entry<String, String> param : params.entrySet()) {
+                pairList.add(new BasicNameValuePair(param.getKey(), param.getValue()));
             }
 
-            HttpEntity requestHttpEntity = new UrlEncodedFormEntity(pairList);
-
-//            StringEntity stringEntity = new StringEntity(pairList.toString());
-//            stringEntity.setContentEncoding("UTF-8");
-
-
+            HttpEntity requestHttpEntity = new UrlEncodedFormEntity(pairList, "UTF-8");
             // URL使用基本URL即可，其中不需要加参数
             HttpPost httpPost = new HttpPost(url);
-            httpPost.addHeader("Content-Type","application/x-www-form-urlencoded;charset=utf-8");
-
             // 将请求体内容加入请求中
             httpPost.setEntity(requestHttpEntity);
 
-            RequestConfig config=RequestConfig.custom()
-                             .setConnectTimeout(5000)       // 设置连接超时时间 5秒钟
-                             .setSocketTimeout(5000)        // 设置读取超时时间5秒钟
-                             .build();
+            RequestConfig config = RequestConfig.custom()
+                    .setConnectTimeout(timeout)       // 设置连接超时时间 5秒钟
+                    .setSocketTimeout(timeout)        // 设置读取超时时间5秒钟
+                    .build();
             httpPost.setConfig(config);
 
-//            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-//            builder.setCharset(Charset.forName(HTTP.UTF_8));//设置请求的编码格式
-//            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);//设置浏览器兼容模式
-
-//            TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
-//            SSLSocketFactory sf = new SSLSocketFactory(
-//                    acceptingTrustStrategy, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-//            SchemeRegistry registry = new SchemeRegistry();
-//            registry.register(new Scheme("https", 8443, sf));
-//            ClientConnectionManager ccm = new PoolingClientConnectionManager(registry);
-
             // 需要客户端对象来发送请求
-//            BasicHttpParams httpParams = new BasicHttpParams();
-//            HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
-//            HttpProtocolParams.setContentCharset(httpParams, "utf-8");
-//            httpParams.setBooleanParameter("http.protocol.expect-continue", false);
-//            HttpClient httpClient = new DefaultHttpClient(httpParams);
-
             HttpClient httpClient = new DefaultHttpClient();
-
             // 发送请求
             HttpResponse response = httpClient.execute(httpPost);
 
-            String data = EntityUtils.toString(response.getEntity());
+            String data = EntityUtils.toString(response.getEntity(), "UTF-8");
 
             httpClientEventListener.onSuccess(200, data.getBytes());
 
-            Log.d("response" , response.toString());
+            Log.d("response", response.toString());
 
         } catch (Exception error) {
 
             if (error instanceof SocketException) {
                 httpClientEventListener.onTimeout();
-            } else if(error instanceof ConnectException){
+            } else if (error instanceof ConnectException) {
                 httpClientEventListener.onTimeout();
-            }else {
+            } else {
                 httpClientEventListener.onFailure(100, null);
             }
 
         }
 
+    }
+
+
+    /**
+     * 同步Http请求
+     */
+    static class GetStnc2Task implements Runnable {
+
+        private String url;
+        private Map<String, String> params;
+        private int timeout;
+        private HttpClientEventListener httpClientEventListener;
+
+        GetStnc2Task(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
+
+            this.url = url;
+            this.params = params;
+            this.timeout = timeout;
+            this.httpClientEventListener = httpClientEventListener;
+
+        }
+
+        @Override
+        public void run() {
+            getSync2(url, params, httpClientEventListener, timeout);
+        }
+
+    }
+
+
+    /**
+     * Http get 异步请求
+     *
+     * @param url
+     * @param params
+     * @param httpClientEventListener
+     * @param timeout
+     */
+    static public void get2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
+        GetStnc2Task getStnc2Task = new GetStnc2Task(url, params, httpClientEventListener, timeout);
+        new Thread(getStnc2Task).start();
+    }
+
+    /**
+     * HttpGet 同步请求
+     *
+     * @param url
+     * @param params
+     * @param httpClientEventListener
+     * @param timeout
+     */
+    static public void getSync2(String url, Map<String, String> params, final HttpClientEventListener httpClientEventListener, int timeout) {
+        try {
+            List<NameValuePair> pairList = new ArrayList<NameValuePair>();
+            for (Map.Entry<String, String> param : params.entrySet()) {
+                pairList.add(new BasicNameValuePair(param.getKey(), param.getValue()));
+            }
+
+            HttpEntity requestHttpEntity = new UrlEncodedFormEntity(pairList, "UTF-8");
+            // URL使用基本URL即可，其中不需要加参数
+            HttpGet httpGet = new HttpGet(url);
+            // 将请求体内容加入请求中
+            httpGet.setEntity(requestHttpEntity);
+
+            RequestConfig config = RequestConfig.custom()
+                    .setConnectTimeout(timeout)       // 设置连接超时时间 5秒钟
+                    .setSocketTimeout(timeout)        // 设置读取超时时间5秒钟
+                    .build();
+            httpGet.setConfig(config);
+
+            // 需要客户端对象来发送请求
+            HttpClient httpClient = new DefaultHttpClient();
+            // 发送请求
+            HttpResponse response = httpClient.execute(httpGet);
+
+            String data = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+            httpClientEventListener.onSuccess(200, data.getBytes());
+
+            Log.d("response", response.toString());
+
+        } catch (Exception error) {
+
+            if (error instanceof SocketException) {
+                httpClientEventListener.onTimeout();
+            } else if (error instanceof ConnectException) {
+                httpClientEventListener.onTimeout();
+            } else {
+                httpClientEventListener.onFailure(100, null);
+            }
+
+        }
+    }
+
+    /**
+     * 附件上传 同步的方式
+     *
+     * @param context
+     * @param url
+     * @param params
+     * @param httpClientEventListener
+     * @param timeout
+     */
+    static public void PostJsonSync(Context context, String url, Map<String, Object> params, final HttpClientEventListener httpClientEventListener, int timeout) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost httppost = new HttpPost(url);
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(200000).setSocketTimeout(200000).build();
+            httppost.setConfig(requestConfig);
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            for (Map.Entry<String, Object> param : params.entrySet()) {
+                Object val = param.getValue();
+                if(val instanceof File){
+                    builder.addPart(param.getKey(),new FileBody((File) val));
+                }else{
+                    builder.addPart(param.getKey(),new StringBody(param.getValue().toString(),ContentType.TEXT_PLAIN));
+                }
+            }
+            HttpEntity reqEntity = builder.build();
+            httppost.setEntity(reqEntity);
+            long s = System.currentTimeMillis();
+            Log.e("test","begin ->" + s);
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            long e = System.currentTimeMillis();
+            Log.e("test","time cost ->" + (e -s));
+            try {
+                HttpEntity resEntity = response.getEntity();
+                int statusCode = response.getStatusLine().getStatusCode();
+                Log.e("test","statusCode -> " + statusCode);
+                if (statusCode == 200 && resEntity != null) {
+                    String data = EntityUtils.toString(response.getEntity(), "UTF-8");
+                    httpClientEventListener.onSuccess(200, data.getBytes());
+                    Log.e("test","onSuccess --> " + data);
+                }
+                EntityUtils.consume(resEntity);
+            } finally {
+                response.close();
+            }
+        } catch (Exception error) {
+            if (error instanceof SocketException) {
+                httpClientEventListener.onTimeout();
+            } else if (error instanceof ConnectException) {
+                httpClientEventListener.onTimeout();
+            } else {
+                httpClientEventListener.onFailure(100, null);
+            }
+        } finally {
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                Log.e("test",e.getMessage(),e);
+            }
+            Log.e("test","上传成功");
+        }
     }
 
 }
